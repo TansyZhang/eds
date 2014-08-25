d94f3b76ac44fa75d8652d18db56aa36ff96b839
yeguangsuixing <411707420@qq.com>
2014-08-26 07:50:14 +0800

    Signed-off-by: yeguangsuixing <411707420@qq.com>

---
+++ Application/Home/Controller/EdsController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class EdsController extends Controller {
+
+	public function __construct(){
+		parent::__construct();
+    	$this->page_title = "全国物理拔尖人才培养计划";
+	}
+	
+}
+
+?>
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,11 +1,18 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends EdsController {
 
+	public function __construct(){
+		parent::__construct();
+		$this->phy_news_title = '物理前沿';
+		$this->hot_act_title = '热门活动';
+		$this->hot_post_title = '学生社区热点';
+		$this->recmd_course_title = '推荐课程';
+		$this->more = '更多>>';
+	}
 	
     public function index(){
-    	$this->page_title = "全国物理拔尖人才培养计划";
         $this->display();
     }
 }
